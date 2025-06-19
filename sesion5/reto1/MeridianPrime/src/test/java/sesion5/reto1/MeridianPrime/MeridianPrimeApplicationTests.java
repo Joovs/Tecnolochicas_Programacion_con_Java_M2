2e56512c98e08929def19534d0fab6a1093320ab
Joovs <jovannaescogar9@gmail.com>
2025-06-18 18:21:38 -0600

    Sesión 5 reto 1

---
+++ sesion5/reto1/MeridianPrime/src/test/java/sesion5/reto1/MeridianPrime/MeridianPrimeApplicationTests.java
@@ -0,0 +1,13 @@
+package sesion5.reto1.MeridianPrime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeridianPrimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
